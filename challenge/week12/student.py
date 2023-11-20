baea4938b6e63aa858b6a05c0e52d550d5191cd4
mine-park <a46763683@gmail.com>
2023-11-20 18:34:04 +0900

    week12

---
+++ challenge/week12/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, kor, math, eng): # 생성자 작성
+        self.name = name # 이름 초기화
+        self.kor = kor # 국어 초기화
+        self.math = math # 수학 초기화
+        self.eng = eng # 영어 초기화
